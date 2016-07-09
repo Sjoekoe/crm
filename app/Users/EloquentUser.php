89dafbded0e774fac3ea7bf1091a2f42149c5d3f
Hans Jonckers <jonckershans@gmail.com>
2016-07-09 10:13:48 +0200

    First commit

---
+++ app/Users/EloquentUser.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Users;
+
+use App\Models\StandardModel;
+use Carbon\Carbon;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class EloquentUser extends Authenticatable implements User
+{
+    use StandardModel;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * @return string
+     */
+    public function name()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function email()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return \Carbon\Carbon
+     */
+    public function birthDay()
+    {
+        return Carbon::parse($this->birthday);
+    }
+
+    /**
+     * @return string
+     */
+    public function password()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return string
+     */
+    public function rememberToken()
+    {
+        return $this->remember_token;
+    }
+}
---
+++ app/Users/User.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Users;
+
+interface User
+{
+    /**
+     * @return int
+     */
+    public function id();
+
+    /**
+     * @return string
+     */
+    public function name();
+
+    /**
+     * @return string
+     */
+    public function email();
+
+    /**
+     * @return \Carbon\Carbon
+     */
+    public function birthDay();
+
+    /**
+     * @return string
+     */
+    public function password();
+
+    /**
+     * @return string
+     */
+    public function rememberToken();
+
+    /**
+     * @return \Carbon\Carbon
+     */
+    public function createdAt();
+
+    /**
+     * @return \Carbon\Carbon
+     */
+    public function updatedAt();
+}
